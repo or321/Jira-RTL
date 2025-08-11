709ee8e27968e5482510d70069e9fb7a78e3429b
Or Liran <orlir123@gmail.com>
2025-08-11 07:46:32 +0300

    Added several selectors to the observer rules script. Those selectors now apply RTL to several new components, including:
    releases view, work items under release, issue relationship tables, issue hover-cards, modal dialog of search results, search inputs.
    Added comments to explain what each selector represents in the Jira website.
    
    Updated the extension css file - for code blocks, preserve LTR direction. For the main search input, set padding when RTL is applied to it.

--- src/scripts/observerRules.js
+++ src/scripts/observerRules.js
@@ -8,21 +8,34 @@ import { ResolveText } from "./enums/ResolveText";
  * @type {Array<{ selectors: string[], compiledSelector: string, behavior: Behavior, resolveTarget: ResolveTarget, resolveText: ResolveText }>}
  */
 export const observerRules = [
+	/* Simple elements where RTL is applied directly */
 	{
 		selectors: [
-			/* Jira issue view (page or modal) */
+			// Issue view - the issue summary
 			'[data-testid="issue-field-summary.ui.issue-field-summary-inline-edit--container"]',
 
-			'[data-component-selector="jira-issue-view-rich-text-inline-edit-view-container"] .ak-renderer-document p',
-			'[data-component-selector="jira-issue-view-rich-text-inline-edit-view-container"] .ak-renderer-document li',
+			// Elements which on click turn to editable with the Jira text editor.
+			// This includes jira issue description, comments in the issue activity section, and more
+			'.ak-renderer-document p',
+			'.ak-renderer-document li',
 
-			'[data-testid^="issue-comment-base.ui.comment.ak-comment."][data-testid$="-body"] .ak-renderer-document p',
-			'[data-testid^="issue-comment-base.ui.comment.ak-comment."][data-testid$="-body"] .ak-renderer-document li',
-
-			/* Cards in Sprint or Kanban board view */
+			// Cards in Sprint or Kanban board view
 			'[data-component-selector="platform-card.ui.card.card-content.content-section"]',
 
-			/* Tooltips */
+			// Versions/releases table view
+			'[data-testid="project-directories.versions.main.table.table-container"] table > tbody > tr > td',
+
+			// Work items table view for a specific version/release
+			'[data-testid="software-releases-version-detail-issue-list.ui.issues.issue-card"] div:first-child > div[role="presentation"]',
+
+			// Issue description inside an issue-relationship table
+			'[data-testid="issue-field-summary.ui.inline-read.link-item--primitive--container"]',
+
+			// Content inside an issue hover-card
+			'[data-testid="hover-card"] [data-smart-element="Title"]',
+			'[data-testid="hover-card"] [data-testid="smart-element-text"]',
+
+			// Tooltips
 			'.atlaskit-portal-container [role="tooltip"]',
 		],
 		behavior: Behavior.DEFAULT,
@@ -30,9 +43,9 @@ export const observerRules = [
 		resolveText: ResolveText.INNER_TEXT
 	},
 
+	/* Issues table view - the cells containing the issues summary */
 	{
 		selectors: [
-			/* Issue table view */
 			'table[data-vc="issue-table"] [data-testid="native-issue-table.common.ui.issue-cells.issue-summary.issue-summary-cell"]'
 		],
 		behavior: Behavior.DEFAULT,
@@ -40,22 +53,51 @@ export const observerRules = [
 		resolveText: ResolveText.INNER_TEXT
 	},
 
-	// Editable Fields with content editor
+	/* Header of an issue-relationship table */
+	{
+		selectors: [
+			'[data-testid="issue.issue-view.views.issue-base.content.issue-links.issue-links-view.relationship-heading"]'
+		],
+		behavior: Behavior.DEFAULT,
+		resolveTarget: ResolveTarget.PARENT,
+		resolveText: ResolveText.INNER_TEXT
+	},
+
+	/* Title of issues in the modal dialog of search results */
+	{
+		selectors: [
+			'[data-test-id="search-dialog-dialog-wrapper"] [data-vc="search-result-section"] .SearchDialogResultTitle'
+		],
+		behavior: Behavior.DEFAULT,
+		resolveTarget: ResolveTarget.PARENT,
+		resolveText: ResolveText.INNER_TEXT
+	},
+
+	/* Editable Fields with content editor */
 	{
 		selectors: [
 			'[contenteditable="true"]',
-			'.ak-editor-content-area'
+			'.ak-editor-content-area',
 		],
 		behavior: Behavior.CONTENT_EDITABLE,
 		resolveTarget: ResolveTarget.SELF,
 		resolveText: ResolveText.INNER_TEXT
 	},
 
-	// Input fields
+	/* Input fields */
 	{
 		selectors: [
+			// A generic search field
+			'input[data-testid="searchfield"]',
+
+			// The "summary" field in the modal for creating new issues
 			'input#summary-field',
+
+			// Issue view - the issue summary (in editable state)
 			'[data-testid="issue.views.issue-base.foundation.summary.heading.writeable"] textarea',
+
+			// The main search input at the top of the page
+			'input[data-test-id="search-dialog-input"]',
 		],
 		behavior: Behavior.INPUT,
 		resolveTarget: ResolveTarget.SELF,
